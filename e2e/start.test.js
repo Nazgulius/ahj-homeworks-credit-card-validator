22d56f15b49cf222e192b0c45df3d27ac9b58cd3
Maksim Chalyh <nazgulius@yandex.ru>
2025-01-16 08:34:31 +0300

    add e2e test

---
+++ e2e/start.test.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+
+describe('Page start', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      heanless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+  
+  test('page start', async () => {
+    await page.goto('http://localhost:8080/');
+
+    await page.waitFor('body');
+  });
+
+  // afterAll(async () => {
+  //   await browser.close();
+  // });
+});
